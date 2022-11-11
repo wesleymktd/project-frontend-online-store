663c142d26c28d19b892ac7c952aabb5edafc8f9
devarturmartins <arturvl1@hotmail.com>
2022-11-11 16:10:03 -0300

    req. 10 em andamento

--- src/pages/Carrinho.jsx
+++ src/pages/Carrinho.jsx
@@ -4,6 +4,7 @@ import ItemCart from '../components/ItemCart';
 export default class Carrinho extends Component {
   state = {
     cart: [],
+    quantidade: 1,
   };
 
   componentDidMount() {
@@ -16,6 +17,22 @@ export default class Carrinho extends Component {
       cart,
     });
   };
+
+  incrementItem = (event) => {
+    const { quantidade } = this.state;
+    this.setState((prev) => ({ quantidade: prev.quantidade + 1 }), () => {
+      const storage = JSON.parse(localStorage.getItem('cart'));
+      const itemClicado = storage.filter((itemStorage) => (
+        event.target.name === itemStorage.title));
+      itemClicado[0].quantity += quantidade;
+      // localStorage.setItem('cart', JSON.stringify(itemClicado));
+    });
+  };
+
+  // decrementItem = (event) => {
+  //   const { cart, quantidade } = this.state;
+  //   this.setState((prev) => ({ quantidade: prev.quantidade - 1 }));
+  // };
 
   render() {
     const { cart } = this.state;
@@ -32,6 +49,9 @@ export default class Carrinho extends Component {
               cart.map((item) => (
                 <ItemCart
                   { ...item }
+                  incrementItem={ this.incrementItem }
+                  decrementItem={ this.decrementItem }
+                  removeItem={ this.removeItem }
                   key={ item.id }
                 />
               ))
